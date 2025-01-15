a1416041be980a7bb8f68be09313cb6cef25fb8e
Azzam Alghamdi <azzam.mo.alghamdi@gmail.com>
2025-01-16 00:20:11 +0300

    first commit

--- main.py
+++ main.py
@@ -2,51 +2,26 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from gen_songs import gen_songs, set_user_description
 import gen_playlist
 import secrets
-import redis
-from flask_session import Session
-import os
-from urllib.parse import urlparse
 
-# Flask app setup
 app = Flask(__name__, static_folder='static')
-
-# Setup session (use Redis for session storage in production)
 app.secret_key = secrets.token_hex(16)
-
-# Configure Redis for session storage
-if 'REDIS_URL' in os.environ:
-    # For Heroku deployment, use the Redis URL provided by the Redis add-on
-    redis_url = os.getenv('REDIS_URL')
-    url = urlparse(redis_url)
-    app.config['SESSION_TYPE'] = 'redis'
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
-    app.config['SESSION_KEY_PREFIX'] = 'flask_'
-    app.config['SESSION_REDIS'] = redis.StrictRedis(
-        host=url.hostname,
-        port=url.port,
-        password=url.password,
-        db=0,
-        socket_keepalive=True # 5 seconds timeout
-    )
-
-else:
-    # For local development, use the default Redis configuration
-    app.config['SESSION_TYPE'] = 'redis'
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
-    app.config['SESSION_KEY_PREFIX'] = 'flask_'
-    app.config['SESSION_REDIS'] = redis.StrictRedis(host='localhost', port=6379, db=0)  # For local Redis server
-
-Session(app)
+@app.route('/')
+def home():
+    # Check if the username is already in the session
+    if 'username' not in session:
+        return redirect(url_for('get_username'))
+    return render_template('index.html')
 
 @app.route('/get_username', methods=['GET', 'POST'])
-def home():
-    if 'username' not in session:
-        print("No username in session!")
-        return redirect(url_for('get_username'))
-    print(f"Username from session: {session['username']}")
-    return render_template('index.html')
+def get_username():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username:
+            session['username'] = username  # Store username in the session
+            return redirect(url_for('home'))
+        else:
+            return render_template('get_username.html', error="Please enter a valid username.")
+    return render_template('get_username.html')
 
 @app.route('/process', methods=['POST'])
 def process():
